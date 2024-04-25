b74e40fb4bfc88a4b6d000977caf2a01a56db4a0
argun8394 <argundoganali@gmail.com>
2024-04-25 17:39:00 +0300

    enviroments

--- src/libs/data.ts
+++ src/libs/data.ts
@@ -1,35 +1,54 @@
 import axios, { AxiosResponse } from "axios";
-import {Data} from '@/types/index'
+import { Data } from "@/types/index";
+import { md5 } from "js-md5";
+
+let API_BASE_URL = "";
+let API_PUBLIC_KEY = "";
+let API_PRIVATE_KEY = "";
+
+if (process.env.NODE_ENV === "development") {
+  API_BASE_URL = process.env.API_BASE_URL_DEV ?? "";
+  API_PUBLIC_KEY = process.env.API_PUBLIC_KEY_DEV ?? "";
+  API_PRIVATE_KEY = process.env.API_PRIVATE_KEY_DEV ?? "";
+} else {
+  API_BASE_URL = process.env.API_BASE_URL_DEV_PROD ?? "";
+  API_PUBLIC_KEY = process.env.API_PUBLIC_KEY_DEV_PROD ?? "";
+  API_PRIVATE_KEY = process.env.API_PRIVATE_KEY_DEV_PROD ?? "";
+}
+
+const getTimeStamp = () => Date.now().toString();
+const getHash = (timeStamp: string) =>
+  md5(timeStamp + API_PRIVATE_KEY + API_PUBLIC_KEY);
+
+const timeStamp = getTimeStamp();
+const hash = getHash(timeStamp);
+const query = `ts=${timeStamp}&apikey=${API_PUBLIC_KEY}&hash=${hash}`;
 
 export const getData = async (
   nameStartsWith?: string,
   offset?: number,
   limit?: number,
-  characterId?:number
+  characterId?: number
 ): Promise<Data> => {
   try {
-      let url = null
-     
-      if(characterId){
-        console.log('first')
+    let url = null;
 
-        url = `https://gateway.marvel.com/v1/public/characters/${characterId}?ts=1&apikey=3579453ac950b98ceed0d384978790d4&hash=d578505f73327bda014280c1f938d0d5`
-        
-      }else if(nameStartsWith){
-        url =`https://gateway.marvel.com/v1/public/characters?ts=1&nameStartsWith=${nameStartsWith}&offset=${offset}&limit=${limit}&apikey=3579453ac950b98ceed0d384978790d4&hash=d578505f73327bda014280c1f938d0d5`;
-      } else {
-        url =`https://gateway.marvel.com/v1/public/characters?ts=1&offset=${offset}&limit=${limit}&apikey=3579453ac950b98ceed0d384978790d4&hash=d578505f73327bda014280c1f938d0d5`;
-
-      }
-     
-      const response: AxiosResponse<Data> = await axios.get(url);
-      if (response && response.data) {
-        return response.data;
-      } else {
-        throw new Error("Failed to fetch data");
-      }
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-      throw error; 
+    if (characterId) {
+      url = `${API_BASE_URL}/${characterId}?${query}`;
+    } else if (nameStartsWith) {
+      url = `${API_BASE_URL}?nameStartsWith=${nameStartsWith}&offset=${offset}&limit=${limit}&${query}`;
+    } else {
+      url = `${API_BASE_URL}?&offset=${offset}&limit=${limit}&${query}`;
     }
-  };
+
+    const response: AxiosResponse<Data> = await axios.get(url);
+    if (response && response.data) {
+      return response.data;
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    throw error;
+  }
+};
