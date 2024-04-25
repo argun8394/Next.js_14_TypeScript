008ec5be29a5f7945ad8d89ce5a77ad03bced12d
argun8394 <argundoganali@gmail.com>
2024-04-26 01:34:54 +0300

    up

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center gap-4 text-xl">
       <h2>Not Found</h2>
       <p>Sorry, the page you are lookingfor does not exist.</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/" className="underline text-red-700">
+        Return Home
+      </Link>
     </div>
   );
 };
